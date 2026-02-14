316bdfad9c0fa2c7a7ca641bce333dae35c0bd0e
youssefGaddes <Youssef.Gaddes@esprit.tn>
2026-02-14 15:07:04 +0100

    Delete src directory

--- src/main/java/tn/esprit/utils/UserContext.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.utils;
-
-public class UserContext {
-    private static Long walletId;
-
-    public static Long getWalletId() { return walletId; }
-    public static void setWalletId(Long id) { walletId = id; }
-    public static void clear() { walletId = null; }
-}
